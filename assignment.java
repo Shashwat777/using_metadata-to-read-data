148d60e750ba6db97d482b950759181a7b6aebbb
shashwat <shashwatjain2014@gmail.com>
2019-01-22 17:26:03 +0530

    Hey lakra
    Please have a look !!

---
+++ assignment.java
@@ -0,0 +1,117 @@
+import java.io.File;
+import java.util.Scanner;
+import java.lang.Object;
+import java.io.OutputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+
+public class assignment {
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      String fn=sc.next(); /*FIELD NAME INPUT*/
+      int notexist=0;  /*SET TO ZERO*/
+      int charr=0;
+
+        try {
+          Scanner input = new Scanner(System.in);
+
+          File file = new File("metadata.txt");
+              String[] fielddata=null;
+
+
+          input = new Scanner(file);
+          int position=0; /* position of field in datafile*/
+          int chk=0;
+
+
+                      while (input.hasNextLine()) { /*READS THE META DATA FILE */
+
+
+                          String line = input.nextLine();
+                           String[] arr=line.split(" ") ;
+                           if(arr[0].equals(fn)){ /* WHEN FIRST WORD OF LINE IS SAME AS FIELD NAME */
+                             fielddata=arr; /* FIELD DATA CONTAINS CONTAINS INFORMATION OF OUR INPUT field */
+
+                             position=chk;     /*FIELD NAME POSITION IN DATAFILE */
+
+
+                           }
+                           System.out.println(line);
+                           chk++;
+                      }
+                    System.out.println("Finish reading data description file....");
+                      input.close();
+                      if(fielddata==null){
+
+                      notexist=1;  /*if fielddata equals null*/
+                      }
+                      else{
+                        String type=fielddata[fielddata.length-1];
+                        String[] type_arr=type.split("");
+
+                        if(type_arr[0].equals("C")){  /*to make sure that field is not of type char as max valof char can't be found */
+                        charr=1;
+                        }
+
+
+
+                      }
+                     System.out.println();
+                     Scanner datafile=new Scanner(System.in);
+                      File filedata = new File("datafile.txt");
+                      datafile=new Scanner(filedata);
+                      while (datafile.hasNextLine()) {
+                        String line = datafile.nextLine();
+                        System.out.println(line);}
+                      System.out.println("Find max value in the field " + fn);
+                      if(notexist==1){
+                        System.out.println("--- Error: field name not found");
+                      }
+                      else if(charr==1){
+                        System.out.println("---- Error: field char max cant be found"); /*IF FIELD NAME IS CHAR NO MAX CAN BE FOUND */
+
+                      }
+
+
+
+
+
+
+
+        /*  FileInputStream file1 = new FileInputStream("metadata.txt");
+          ObjectInputStream in = new ObjectInputStream(file1);
+
+          String  db = (String) in.readObject();
+          System.out.println(db);
+
+          in.close();
+          file1.close();*/
+
+          		/*	FileOutputStream file = new FileOutputStream("we.data");
+
+          			ObjectOutputStream out = new ObjectOutputStream(file);
+                String j="hey!!! how are you";
+          			out.writeObject(j);
+          			out.close();
+          			file.close();*/
+
+
+
+
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
